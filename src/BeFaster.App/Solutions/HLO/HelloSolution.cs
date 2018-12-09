355de4940048ddc6cdf644f1ded7d6b27184b47a
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-12-09 08:18:10 +0000

    First commit

---
+++ src/BeFaster.App/Solutions/HLO/HelloSolution.cs
@@ -0,0 +1,12 @@
+﻿using BeFaster.Runner.Exceptions;
+
+namespace BeFaster.App.Solutions.HLO
+{
+    public static class HelloSolution
+    {
+        public static string Hello(string friendName)
+        {
+            return "Hello, "+friendName+"!";
+        }
+    }
+}
