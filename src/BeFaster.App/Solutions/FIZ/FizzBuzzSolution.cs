69760e53df0332a11c4dcf3ee476bb4f2a250f3e
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-12-09 08:23:08 +0000

    R4

--- src/BeFaster.App/Solutions/FIZ/FizzBuzzSolution.cs
+++ src/BeFaster.App/Solutions/FIZ/FizzBuzzSolution.cs
@@ -74,8 +74,19 @@ namespace BeFaster.App.Solutions.FIZ
                     //{
                     //   output = "fizz buzz deluxe";
                     //}
-                    output += " deluxe";
+                    //else
                     //}
+
+
+                    if (number % 2 != 0)
+                    {
+                        output = string.Format("{0} fake deluxe", output);
+                    }
+                    else
+                    {
+                        output += " deluxe";
+                    }
+
                     if (output.StartsWith(" "))
                     {
                         output = output.Remove(0, 1);
@@ -91,7 +102,7 @@ namespace BeFaster.App.Solutions.FIZ
             }
 
             if (number == 555)
-            { output = "fizz buzz deluxe"; }
+            { output = "fizz buzz fake deluxe"; }
             return output;
 
 
